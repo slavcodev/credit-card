7c48a6fea09d4fc5a540646a0929820522dfadfd
Veaceslav Medvedev <slavcopost@gmail.com>
2024-07-10 18:21:33 +0200

    Fix test failed in new PhpUnit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,4 +4,9 @@ namespace LVR\CreditCard\Tests;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    // Implement compatibility with new PhpUnit that requires first constructor argument.
+    public function __construct($name = null)
+    {
+        parent::__construct($name ?? get_class($this));
+    }
 }
